d01dfd9cd8d6099121bdb8a0946a4b183fe02777
Tadashi Ringo <phamt8611@gmail.com>
2020-09-10 17:36:33 +0700

    Thêm vào đối tượng Age_Object cho hàm Text_Conversation và đồng thời cũng viết thêm một hàm mới,hàm này có chức năng in ra một đoạn văn bản hỏi tên đối tượng

--- Ringo-AI.py
+++ Ringo-AI.py
@@ -1,13 +1,18 @@
 
 class Text_Conversation:
     "Class khai báo những thành phần về chào hỏi."
-    def __init__(self,Name_Object="Người nào đó!"):
+    def __init__(self,Name_Object="Người nào đó",Age_Object=0):
         self.Name_Object = Name_Object
+        self.Age_Object = Age_Obejct
     def Hello(self): 
         print("Xin chào {}! ".format(self.Name_Object))
     def What_Your_Name(self):
         print("Tên của bạn là gì?")
+    def How_Old_Are_You(self):
+        print("Bạn bao nhiêu tuổi?")
 
 #names = input("Tên người trước mặt: ")
 Name = Text_Conversation()
 Name.What_Your_Name()
+g
+
