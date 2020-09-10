e6ed929bd49b96b1c2bf0bbb104eaee1723086b1
Tadashi Ringo <phamt8611@gmail.com>
2020-09-10 17:12:16 +0700

    Thêm vào một hàm mới vào trong class Text_Conversation, với tên hàm là What_Your_Name - Chức năng của nó là hỏi tên của người trước mắt là cái gìgit add Ringo-AI.py!

--- Ringo-AI.py
+++ Ringo-AI.py
@@ -1,12 +1,13 @@
 
 class Text_Conversation:
     "Class khai báo những thành phần về chào hỏi."
-    def __init__(self,Name_Object):
+    def __init__(self,Name_Object="Người nào đó!"):
         self.Name_Object = Name_Object
     def Hello(self): 
         print("Xin chào {}! ".format(self.Name_Object))
+    def What_Your_Name(self):
+        print("Tên của bạn là gì?")
 
-
-names = input("Tên người trước mặt: ")
-Name = Text_Conversation(names)
-Name.Hello()
+#names = input("Tên người trước mặt: ")
+Name = Text_Conversation()
+Name.What_Your_Name()
