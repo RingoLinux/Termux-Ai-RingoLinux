b5e38f2f044f4ecbe5dfb2f0ec48fdcbcda40833
Tadashi Ringo <phamt8611@gmail.com>
2020-09-11 08:53:42 +0700

    Đã hoàn thiện cấu trúc if else là nếu đúng thì đăng nhập nếu sai thì không

--- Ringo-AI.py
+++ Ringo-AI.py
@@ -1,3 +1,7 @@
+import os
+import time
+
+
 
 class Text_Conversation:
     "Class khai báo những thành phần về chào hỏi."
@@ -14,18 +18,37 @@ class Text_Conversation:
 class Ringo_System:
     "Class khai báo những thành phần về hệ thống trong termux "
     def __init__(self,User_Login="admin",Password_Login="admin",Login_Switch="Off"):
+
+        #From_Login_Object
         self.User_Login = User_Login
-        self.Pasword_Login = Password_Login
+        self.Password_Login = Password_Login
         self.Login_Switch = Login_Switch
+    
+
     def From_Login(self):
         if self.Login_Switch == "Off":
             print("Login không được bật,bạn có muốn mở nó lên hay không?") 
-            self.Login_Switch_YN = input("Lựa chọn y/n")
+            self.Login_Switch_YN = input("Lựa chọn y/n: ")
             if self.Login_Switch_YN == "y" or self.Login_Switch_YN == "Y":
                 self.Login_Switch = "On"
-                print(self.Login_Switch)
+                print("Công tắc đã được " + self.Login_Switch)
+                time.sleep(1)
+                os.system("clear")
             else:
                 print("Bạn đã lựa chọn từ chối!!")
+                time.sleep(1)
+                os.system("clear")
+        if self.Login_Switch == "On":
+            User_Player = input("username: ")
+            Password_Player = input("pasword: ")
+            if User_Player == self.User_Login and Password_Player == self.Password_Login:
+                print("Bạn đã đăng nhập thành công")
+                time.sleep(1)
+                os.system("clear")
+            else:
+                print("Bạn đã đăng nhập thất bại")
+                time.sleep(1)
+                os.system("clear")
 
 a = Ringo_System()
 a.From_Login()
