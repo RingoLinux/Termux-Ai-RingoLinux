29ec325b5ae3d8dab387c5f85c42525a4bae7fca
Tadashi Ringo <phamt8611@gmail.com>
2020-09-11 08:17:19 +0700

    Thêm vào hàm From_Login và phần công tắc có bật Login hay không. - Mục đích: Để cho người dùng có thể thuận tiện hơn cho việc tắt và mở login, cung cấp một sự lựa chọn tùy ý

--- Ringo-AI.py
+++ Ringo-AI.py
@@ -11,7 +11,21 @@ class Text_Conversation:
     def How_Old_Are_You(self):
         print("Bạn bao nhiêu tuổi?")
 
-#names = input("Tên người trước mặt: ")
-Name = Text_Conversation()
-Name.What_Your_Name()
+class Ringo_System:
+    "Class khai báo những thành phần về hệ thống trong termux "
+    def __init__(self,User_Login="admin",Password_Login="admin",Login_Switch="Off"):
+        self.User_Login = User_Login
+        self.Pasword_Login = Password_Login
+        self.Login_Switch = Login_Switch
+    def From_Login(self):
+        if self.Login_Switch == "Off":
+            print("Login không được bật,bạn có muốn mở nó lên hay không?") 
+            self.Login_Switch_YN = input("Lựa chọn y/n")
+            if self.Login_Switch_YN == "y" or self.Login_Switch_YN == "Y":
+                self.Login_Switch = "On"
+                print(self.Login_Switch)
+            else:
+                print("Bạn đã lựa chọn từ chối!!")
 
+a = Ringo_System()
+a.From_Login()
